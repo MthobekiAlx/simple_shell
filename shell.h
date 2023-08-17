462007cc2454fe2e5e0c23de3f0a70274b3997a8
Mthobeki <mthobekisikhosang@gmail.com>
2023-08-17 14:55:52 +0200

    first instance of my shell.h file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#define SHELL_H
+#ifndef SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <wait.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+//included almost all the libraries that I have come accross and please dont mind if/when some of them dont get used in the entirety(spelling) of the programm
+
+#include "marcos.h" //remember this one from this other project
