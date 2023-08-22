25d89e612f199a730e226ad059ca3d3f9bf85b90
Mthobeki <mthobekisikhosang@gmail.com>
2023-08-22 16:46:15 +0200

    shell.h funtion

--- main.c
+++ main.c
@@ -1,18 +1,35 @@
 #include "shell.h"
 
-//main .c file that creates a simple shell
 /*
+ * main .c file that creates a simple shell
  * @av - argument vector
  * @ac - argument count
- * On succes return 0
+ * On success, return 0
  */
 
-int main(int ac, char **av, char **env)
-{
-	(void)av;
-	(void)ac;
+int main(int ac, char **av, char **env) {
+    (void) av;
+    (void) ac;
 
-	prompt(env);
+    char *line = NULL;
+    size_t n = 0;
 
-	return (0);
-}		
+    while (1) {
+        printf("Shell Prompt> ");
+        ssize_t chars_read = custom_getline(&line, &n, stdin);
+
+        if (chars_read == -1) {
+            perror("Error reading input");
+            free(line);
+            continue;
+        }
+
+        // Process the user input here
+        // Implement your shell logic to parse and execute commands
+
+        free(line);
+    }
+
+    return 0;
+}
+
