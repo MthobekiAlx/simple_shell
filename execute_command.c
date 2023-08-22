7a33eafc85331a95f2d91fbe5a5281c54b567d2f
Mthobeki <mthobekisikhosang@gmail.com>
2023-08-22 19:14:21 +0200

    execute file

---
+++ execute_command.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+int execute_command(program_data *data)
+{
+    int retval = 0, status;
+    pid_t pidd;
+
+    retval = builtin_list(data);
+    if (retval != -1)
+        return (retval);
+
+    retval = find_program(data);
+    if (retval)
+    {
+        return (retval);
+    }
+    else
+    {
+        pidd = fork();
+        if (pidd == -1)
+        {
+            perror(data->command_name);
+            exit(EXIT_FAILURE);
+        }
+        if (pidd == 0)
+        {
+            retval = execve(data->tokens[0], data->tokens, data->env);
+            if (retval == -1)
+                perror(data->command_name), exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+            if (WIFEXITED(status))
+                errno = WEXITSTATUS(status);
+            else if (WIFSIGNALED(status))
+                errno = 128 + WTERMSIG(status);
+        }
+    }
+    return (0);
+}
+
