578f612478cf60076e9de822ab77da584d7edbc0
MthobekiAlx <122866719+MthobekiAlx@users.noreply.github.com>
2023-08-24 23:32:14 +0200

    Update p_helper.c

--- p_helper.c
+++ p_helper.c
@@ -1,20 +1,20 @@
 #include "shell.h"
 
 /**
- * _print - writes a array of chars in the standar output
+ * _print - writes a array of chars in the stdoutput
  * @string: pointer to the array of chars
  * Return: the number of bytes writed or .
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned
  */
 int _print(char *string)
 {
 	return (write(STDOUT_FILENO, string, str_length(string)));
 }
 /**
- * _printe - writes a array of chars in the standar error
+ * _printe - writes a array of chars in the std error
  * @string: pointer to the array of chars
  * Return: the number of bytes writed or .
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned.
  */
 int _printe(char *string)
 {
@@ -22,11 +22,11 @@ int _printe(char *string)
 }
 
 /**
- * _print_error - writes a array of chars in the standart error
+ * _print_error - writes a array of chars in the std error
  * @data: a pointer to the program's data'
  * @errorcode: error code to print
  * Return: the number of bytes writed or .
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned.
  */
 int _print_error(int errorcode, program_data *data)
 {
