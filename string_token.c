7b2560d1a2a3713b3fc05774192874dd05c8ab4a
MthobekiAlx <122866719+MthobekiAlx@users.noreply.github.com>
2023-08-24 22:20:44 +0200

    Update string_token.c

--- string_token.c
+++ string_token.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 /**
  * _strtok - separates strings with delimiters
- * @line: It´s pointer to array we receive in getline.
- * @delim: It´s characters we mark off string in parts.
- * Return: A pointer to the created token
+ * @line: It´s pointer to getline.
+ * @delim: It´s characters we mark off.
+ * Return: A pointer to the created token.
 */
 char *_strtok(char *line, char *delim)
 {
